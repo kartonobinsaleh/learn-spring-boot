ab41d7a721df067a423fd4697c9f2b99488d9df9
kartonobinsaleh <kartono.saleh@gmail.com>
2025-09-04 21:19:48 +0700

    chore: remove domain app

--- src/main/java/com/myapp/DemoApiApplication.java
+++ src/main/java/com/myapp/DemoApiApplication.java
@@ -1,4 +1,4 @@
-package com.myapp.app;
+package com.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/myapp/DemoApiApplicationTests.java
+++ src/test/java/com/myapp/DemoApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.myapp.app;
+package com.myapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
