0a0783e71d80f0a4182daade07e03c1c0271a0bb
kartonobinsaleh <kartono.saleh@gmail.com>
2025-09-05 22:19:52 +0700

    chore(repo): add for find by name

--- src/main/java/com/myapp/models/repos/ProductRepo.java
+++ src/main/java/com/myapp/models/repos/ProductRepo.java
@@ -1,9 +1,11 @@
 package com.myapp.models.repos;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.myapp.models.entities.Product;
 
 public interface ProductRepo extends CrudRepository<Product, Long> {
-
+    List<Product> findByNameContains(String name);
 }
