e641ff995c8c070c1fbfdf8570ffcf5402bd3e60
kartonobinsaleh <kartono.saleh@gmail.com>
2025-09-05 22:15:34 +0700

    feat(service): add ProductService with basic CRUD operations

---
+++ src/main/java/com/myapp/services/ProductService.java
@@ -0,0 +1,33 @@
+package com.myapp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.myapp.models.entities.Product;
+import com.myapp.models.repos.ProductRepo;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public Product save(Product product) {
+        return productRepo.save(product);
+    }
+
+    public Product findOne(Long id) {
+        return productRepo.findById(id).get();
+    }
+
+    public Iterable<Product> findAll() {
+        return productRepo.findAll();
+    }
+
+    public void removeOne(Long id) {
+        productRepo.deleteById(id);
+    }
+}
